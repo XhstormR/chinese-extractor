1526ba522f39b843c5a376b8264af954eeaa37e2
XhstormR <10527522+XhstormR@users.noreply.github.com>
2020-08-25 11:31:28 +0800

    Code refine
    Signed-off-by: XhstormR <10527522+XhstormR@users.noreply.github.com>

--- src/main/kotlin/com/xhstormr/app/WhiteList.kt
+++ src/main/kotlin/com/xhstormr/app/WhiteList.kt
@@ -1,38 +1,9 @@
 package com.xhstormr.app
 
 import com.hankcs.algorithm.AhoCorasickDoubleArrayTrie
+import com.hankcs.hanlp.HanLP
 
 object WhiteList {
-
-    val characters_s = getSystemResourceAsStream("characters_s.txt")
-        .bufferedReader()
-        .use { it.readText() }
-        .toCharArray()
-
-    val characters_t = getSystemResourceAsStream("characters_t.txt")
-        .bufferedReader()
-        .use { it.readText() }
-        .toCharArray()
-
-    val characters = characters_s.toMutableSet()
-        .apply { addAll(characters_t.asIterable()) }
-        .toCharArray()
-
-    val words_s = getSystemResourceAsStream("words_s.txt")
-        .bufferedReader()
-        .readLines()
-        .toSet()
-
-    val words_t = getSystemResourceAsStream("words_t.txt")
-        .bufferedReader()
-        .readLines()
-        .toSet()
-
-    val words_trie = words_s.toMutableSet()
-        .apply { addAll(words_t.asIterable()) }
-        .toSet()
-        .associateBy { it }
-        .let { AhoCorasickDoubleArrayTrie<String>().apply { build(it) } }
 
     val domain = getSystemResourceAsStream("domain.txt")
         .bufferedReader()
@@ -42,29 +13,50 @@ object WhiteList {
         .bufferedReader()
         .use { it.readText() }
 
-    val cet_trie = buildDictTrie("cet.txt")
+    val characters_s = getSystemResourceAsStream("characters_s.txt")
+        .bufferedReader()
+        .use { it.readText() }
 
-    val pinyin_word_trie = buildDictTrie("pinyin_word.txt")
+    val words_s = getSystemResourceAsStream("words_s.txt")
+        .bufferedReader()
+        .readLines()
+        .toSet()
 
-    val vul_number_trie = buildDictTrie("vul_number.txt")
+    val characters = (characters_s + HanLP.s2t(characters_s)).toSet()
 
-    val malicious_trie = buildDictTrie("malicious.txt")
+    val words = words_s + words_s.map { HanLP.s2t(it) }
 
-    val antivirus_trie = buildDictTrie("antivirus.txt")
+    val words_trie by lazy { buildDictTrie(words) }
 
-    val malware_trie = buildDictTrie("malware.txt")
+    val cet_trie by lazy { buildDictTrie("cet.txt") }
 
-    val website_trie = buildDictTrie("website.txt")
+    val pinyin_word_trie by lazy { buildDictTrie("pinyin_word.txt") }
 
-    val local_trie = buildDictTrie("local.txt")
+    val vul_number_trie by lazy { buildDictTrie("vul_number.txt") }
 
-    val software_trie = buildDictTrie("software.txt")
+    val malicious_trie by lazy { buildDictTrie("malicious.txt") }
+
+    val antivirus_trie by lazy { buildDictTrie("antivirus.txt") }
+
+    val malware_trie by lazy { buildDictTrie("malware.txt") }
+
+    val website_trie by lazy { buildDictTrie("website.txt") }
+
+    val local_trie by lazy { buildDictTrie("local.txt") }
+
+    val software_trie by lazy { buildDictTrie("software.txt") }
 
     private fun buildDictTrie(name: String) = getSystemResourceAsStream(name)
         .bufferedReader()
         .readLines()
-        .toSet()
         .map { it.toLowerCase() }
+        .toSet()
+        .associateBy { it }
+        .let { AhoCorasickDoubleArrayTrie<String>().apply { build(it) } }
+
+    private fun buildDictTrie(list: Collection<String>) = list
+        .map { it.toLowerCase() }
+        .toSet()
         .associateBy { it }
         .let { AhoCorasickDoubleArrayTrie<String>().apply { build(it) } }
 }
