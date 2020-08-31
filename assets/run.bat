feff949897bce2027da3628f2c35d49f4bb05b94
XhstormR <10527522+XhstormR@users.noreply.github.com>
2020-08-31 14:04:30 +0800

    Update
    Signed-off-by: XhstormR <10527522+XhstormR@users.noreply.github.com>

--- assets/run.bat
+++ assets/run.bat
@@ -3,3 +3,5 @@ opencc.exe -c s2t.json -i characters_s.txt -o characters_t.txt
 fd -x busybox sh -c "sort -u -o {}.sort {} && mv -f {}.sort {}"
 
 busybox tr [:upper:] [:lower:] < in.txt > out.txt
+
+strings -aoe s main.exe
